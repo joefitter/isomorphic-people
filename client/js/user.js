9ef84a6009444fda036ed4cd6f00cce961367ee9
Joe Fitter <joefitter@hotmail.com>
2015-08-28 15:29:01 +0100

    stuff

--- client/js/user.js
+++ client/js/user.js
@@ -1,8 +1,8 @@
-Template.user.events({
-  'click tr': function() {
-    Router.go('/user/' + this._id);
-  },
-  'click .delete': function() {
-    Users.remove(this._id)
-  }
-});
+// Template.user.events({
+//   'click tr': function() {
+//     Router.go('/user/' + this._id);
+//   },
+//   'click .delete': function() {
+//     Users.remove(this._id)
+//   }
+// });
--- client/js/users.js
+++ client/js/users.js
@@ -1,9 +1,19 @@
-Template.users.helpers({
-  users: function() {
-    return Users.find({}, {
-      sort: {
-        name: 1
-      }
-    });
-  }
-});
+// Template.users.helpers({
+//   users: function() {
+//     return Users.find({}, {
+//       sort: {
+//         name: 1
+//       }
+//     });
+//   }
+// });
+
+
+if (Meteor.isClient) {
+  Template.users.helpers({
+    users: function() {
+      console.log(Meteor.users.find({}))
+      return Meteor.users.find({});
+    }
+  });
+}
